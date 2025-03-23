4b303a394e4e0c4cc94dc6e9a9e48280df0b2d82
Gabriel <ggabrielcunha06@gmail.com>
2025-03-23 14:53:00 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string frase;
+bool temNumero = false;
+
+Console.Write("Oi! Eu me chamo Cebolinha, digite alguma flase para eu lepetir: ");
+
+while (true)
+{
+    frase = Console.ReadLine()!;
+
+    foreach (char c in frase)
+    {
+        if (char.IsDigit(c))
+        {
+            temNumero = true;
+            break;
+        }
+    }
+
+    if (!temNumero) break;
+
+    Console.Write("Digite uma flase coleta (sem númelos): ");
+}
+
+frase = frase.Replace("r", "l").Replace("R", "L");
+
+Console.Write(frase);
